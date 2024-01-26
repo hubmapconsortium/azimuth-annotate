3b08b5090c981ca30b5fd9d2e3e052c6a47597b7
Sean Donahue <seandonahue5311@gmail.com>
2024-01-26 10:11:30 -0500

    Fixing typo in output file name

--- bin/azimuth/extract_rna_secondary_matrix.py
+++ bin/azimuth/extract_rna_secondary_matrix.py
@@ -14,7 +14,7 @@ import muon as mu
 
 def main(secondary_analysis_matrix: Path):
     adata = mu.read(f"{fspath(secondary_analysis_matrix)}/rna") if secondary_analysis_matrix.suffix == ".h5mu" else anndata.read_h5ad(secondary_analysis_matrix)
-    adata.write('secondary_analysis.h5aq:qd')
+    adata.write('secondary_analysis.h5ad')
 
 if __name__ == "__main__":
     p = ArgumentParser()
